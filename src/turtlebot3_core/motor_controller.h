6ef3f7046111c009782f6c38d3feec11e4e5d6b4
Marius Niemenmaa <marius.niemenmaa@outlook.com>
2021-12-27 17:30:24 +0200

    Restructure projectfiles

---
+++ src/turtlebot3_core/motor_controller.h
@@ -0,0 +1,76 @@
+/*******************************************************************************
+* Copyright (C) 2021 Marius Niemenmaa 
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#ifndef MOTOR_CONTROLLER_H
+#define MOTOR_CONTROLLER_H
+
+#include "opencr_pins.h"
+
+namespace LokSrc 
+{
+
+// 8 step unipolar stepper motor control
+class MotorController
+{
+public:
+    MotorController();
+    MotorController(int in1, int in2, int in3, int in4);
+    MotorController(int in1, int in2, int in3, int in4, int limit_switch_fw, int limit_switch_bw);
+
+    void setSpeed(long rpm);
+
+    /**
+     * @brief Tries to step motor forward
+     * 
+     * @param steps to step forward 
+     * @return true if succesfull
+     * @return false if not succesfull (limit switch 1 prevents moving)
+     */
+    bool stepForward(int steps);
+
+    /**
+     * @brief Tries to step motor backward 
+     * 
+     * @param steps to step backward 
+     * @return true if succesfull
+     * @return false if not succesfull (limit switch 2 prevents moving)
+     */
+    bool stepBackward(int steps);
+
+private:
+    void step_(int stepIndex);
+
+    bool limitSwitchAllowsMoving_(int limit_switch_pin);
+
+    int control_in1_;
+    int control_in2_;
+    int control_in3_;
+    int control_in4_;
+
+    int limit_switch_fw_;
+    int limit_switch_bw_;
+
+    const int number_of_steps_ = 8;
+    const int steps_per_revolution_ = 64;
+    int current_step_;
+
+    unsigned long step_delay_; // ms
+    unsigned long last_step_time_;
+}; // class MotorController
+
+} // namespace LokSrc
+
+#endif
---
+++ src/turtlebot3_core/opencr_pins.h
@@ -0,0 +1,67 @@
+/*******************************************************************************
+* Copyright (C) 2021 Marius Niemenmaa 
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#ifndef OPENCR_PINS_
+#define OPENCR_PINS_
+
+// These are also defined in <chip.h> but with different numbering.
+// This numbering matches to GPIO layout. 1 and 2 are left out because they are 3.3V and Ground pins
+#define TB_BDPIN_GPIO_3            50
+#define TB_BDPIN_GPIO_4            51
+#define TB_BDPIN_GPIO_5            52
+#define TB_BDPIN_GPIO_6            53
+#define TB_BDPIN_GPIO_7            54
+#define TB_BDPIN_GPIO_8            55
+#define TB_BDPIN_GPIO_9            56
+#define TB_BDPIN_GPIO_10           57
+#define TB_BDPIN_GPIO_11           58
+#define TB_BDPIN_GPIO_12           59
+#define TB_BDPIN_GPIO_13           60
+#define TB_BDPIN_GPIO_14           61
+#define TB_BDPIN_GPIO_15           62
+#define TB_BDPIN_GPIO_16           63
+#define TB_BDPIN_GPIO_17           64
+#define TB_BDPIN_GPIO_18           65
+#define TB_BDPIN_GPIO_19           66
+#define TB_BDPIN_GPIO_20           67
+
+#define CRANE_MOTOR_DRIVER_IN1 TB_BDPIN_GPIO_3 
+#define CRANE_MOTOR_DRIVER_IN2 TB_BDPIN_GPIO_6 
+#define CRANE_MOTOR_DRIVER_IN3 TB_BDPIN_GPIO_8 
+#define CRANE_MOTOR_DRIVER_IN4 TB_BDPIN_GPIO_10 
+
+#define GRIPPER_L_MOTOR_DRIVER_IN1 TB_BDPIN_GPIO_12 
+#define GRIPPER_L_MOTOR_DRIVER_IN2 TB_BDPIN_GPIO_14
+#define GRIPPER_L_MOTOR_DRIVER_IN3 TB_BDPIN_GPIO_16
+#define GRIPPER_L_MOTOR_DRIVER_IN4 TB_BDPIN_GPIO_18
+
+#define GRIPPER_R_MOTOR_DRIVER_IN1 TB_BDPIN_GPIO_13
+#define GRIPPER_R_MOTOR_DRIVER_IN2 TB_BDPIN_GPIO_15
+#define GRIPPER_R_MOTOR_DRIVER_IN3 TB_BDPIN_GPIO_17
+#define GRIPPER_R_MOTOR_DRIVER_IN4 TB_BDPIN_GPIO_19
+
+#define CRANE_FW_LIMIT_SWITCH_ NOT_DEFINED
+#define CRANE_BW_LIMIT_SWITCH_ NOT_DEFINED
+
+#define GRIPPER_L_FW_LIMIT_SWITCH_ NOT_DEFINED
+#define GRIPPER_L_BW_LIMIT_SWITCH_ NOT_DEFINED
+
+#define GRIPPER_R_FW_LIMIT_SWITCH_ NOT_DEFINED
+#define GRIPPER_R_BW_LIMIT_SWITCH_ NOT_DEFINED
+
+#define NOT_DEFINED -1
+
+#endif
